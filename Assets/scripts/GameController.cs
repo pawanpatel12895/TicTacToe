f16c7b7f4cdd8a340743d018214d9cd5c53853c4
Pawan Patel <pawanpatel12895@gmail.com>
2019-11-17 16:26:22 +0530

    alpha build

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -12,6 +12,36 @@ public class GameController : MonoBehaviour {
     private string playerSide;
     private int moveCount;
 
+    public string GetPlayerSide() { return playerSide; }
+    public void EndTurn()
+    {
+        moveCount++;
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide) GameOver();
+        else if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide) GameOver();
+        else if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide) GameOver();
+        else if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide) GameOver();
+        else if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide) GameOver();
+        else if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide) GameOver();
+        else if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide) GameOver();
+        else if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide) GameOver();
+
+        else if (moveCount >= 9)
+            setGameOverText("Draw!!!");
+        ChangeSides();
+    }
+    public void restartGame()
+    {
+        moveCount = 0;
+        playerSide = "X";
+        setBoardInteractable(true);
+        gameOverTextPanel.SetActive(false);
+
+        for (int i = 0; i < buttonList.Length; i++)
+        {   // set each button with empty string
+            buttonList[i].text = "";
+        }
+    }
+
     void SetGameControllerReferenceOnButtons()
     {
         for (int i = 0; i < buttonList.Length; i++)
@@ -20,52 +50,34 @@ public class GameController : MonoBehaviour {
         }
     }
 
-    public string GetPlayerSide() { return playerSide; }
+
     void ChangeSides()
     {
         playerSide = (playerSide == "X") ? "O" : "X";
     }
-    public void EndTurn() {
 
-        moveCount++;
-        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide) GameOver();
-        if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide) GameOver();
-        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide) GameOver();
-        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide) GameOver();
-        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide) GameOver();
-        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide) GameOver();
-        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide) GameOver();
-        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide) GameOver();
 
-        if (moveCount >= 9)
-            setGameOverText("Draw!!!");
-        ChangeSides();
-    }
     void GameOver()
     {
-        for (int i = 0; i < buttonList.Length; i++)
-        {
-            buttonList[i].GetComponentInParent<Button>().interactable = false;
-        }
+        setBoardInteractable(false);
         setGameOverText(playerSide + " Wins!!!"); 
     }
-    void setGameOverText(string text) {
-
+    void setGameOverText(string text) 
+    {
         gameOverTextPanel.SetActive(true);
         gameOverText.text = text;
     }
-    void resetGame() {
-        playerSide = "X";
-        gameOverTextPanel.SetActive(false);
-        moveCount = 0;
+    void setBoardInteractable(bool isInteractable)
+    {
         for (int i = 0; i < buttonList.Length; i++)
         {
-            buttonList[i].GetComponentInParent<Button>().interactable = true;
-            buttonList[i].text = "";
+            buttonList[i].GetComponentInParent<Button>().interactable = isInteractable;
         }
     }
-    void Awake() {
+
+    void Awake() 
+    {
         SetGameControllerReferenceOnButtons();
-        resetGame();
+        restartGame();
     }
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+# TicTacToe
+https://learn.unity.com/tutorial/creating-a-tic-tac-toe-game-using-only-ui-components?signup=true#5c7f8528edbc2a002053b4c3
